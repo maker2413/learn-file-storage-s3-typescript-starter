4a47da194c62f2cfca5d1592d7667b5af3648751
maker2413 <maker2413@gmail.com>
2026-02-12 20:36:40 -0600

    using cloudfront

--- src/api/videos.ts
+++ src/api/videos.ts
@@ -1,7 +1,7 @@
 import { rm } from "fs/promises";
 import path from "path";
 import { getBearerToken, validateJWT } from "../auth";
-import { getVideo, updateVideo } from "../db/videos";
+import { getVideo, updateVideo, type Video } from "../db/videos";
 import { respondWithJSON } from "./json";
 import { uploadVideoToS3 } from "../s3";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
@@ -49,7 +49,7 @@ export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
   const key = `${aspectRatio}/${videoId}.mp4`;
   await uploadVideoToS3(cfg, key, processedFilePath, "video/mp4");
 
-  const videoURL = `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${key}`;
+  const videoURL = `${cfg.s3CfDistribution}/${key}`;
   video.videoURL = videoURL;
   updateVideo(cfg.db, video);
 
